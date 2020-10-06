a1bf9afd47f1aa33a9e04633483c054ee3a2e8a0
Vaibhav-11-1999 <vaibhavkhot36@gmail.com>
2020-10-06 20:43:17 +0530

    first commit

---
+++ day_1/q3_1_arrays.php
@@ -0,0 +1,11 @@
+<?php
+
+$week = array ("sunday", "monday", "tuesday", "wednesday", "thursday", "friday", "satruday");
+
+    for($i=0;$i<=6;$i++)
+        {
+            echo '<hl>' .$week[$i];
+            echo '<br>';
+        }
+
+?>
